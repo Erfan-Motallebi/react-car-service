b97b698f2f556fe2b1b7c96250aa98d67497181f
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-09-11 19:17:39 +0430

    navbar of the homepage is being created

---
+++ client/src/components/layouts/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+import Navbar from "./Navbar/Navbar";
+
+function Home() {
+  return (
+    <Fragment>
+      <Navbar />
+    </Fragment>
+  );
+}
+
+export default Home;
