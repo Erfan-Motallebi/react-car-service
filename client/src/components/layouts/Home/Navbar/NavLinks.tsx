c7d016ee6b8da1af94e0f34d5dc1d848f7bed52a
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-09-12 14:17:21 +0430

    done with navbar

---
+++ client/src/components/layouts/Home/Navbar/NavLinks.tsx
@@ -0,0 +1,49 @@
+import { FunctionComponent } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const NavLinkWrapper = styled.div``;
+
+const UlWrapper = styled.ul`
+  ${tw`
+        lg:flex
+    `}
+  li {
+    ${tw`
+       max-lg:(pt-8 hover:(transition-all transform[translate(20px)] ))
+       max-lg:transition-all
+       `}
+    a {
+      ${tw`
+         transition
+         duration-500
+         ease-in-out
+         lg:(transition-colors pr-5 border-none p-3 m-1 rounded-2xl)
+         hover:text-link
+        `}
+    }
+  }
+`;
+
+const NavLinks: FunctionComponent = () => {
+  return (
+    <NavLinkWrapper>
+      <UlWrapper>
+        <li>
+          <a href="home">Home</a>
+        </li>
+        <li>
+          <a href="cars">Cars</a>
+        </li>
+        <li>
+          <a href="services">Services</a>
+        </li>
+        <li>
+          <a href="Contact">Contact Us</a>
+        </li>
+      </UlWrapper>
+    </NavLinkWrapper>
+  );
+};
+
+export default NavLinks;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,10 +3,32 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+      "max-lg": { max: "975px" },
+      lg: "975px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1100pxpx",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1336px",
+      // => @media (min-width: 1536px) { ... }
+      "3xl": "1580px",
+    },
+    extend: {
+      textColor: {
+        link: "#F037A5",
+      },
+    },
   },
   variants: {
     extend: {},
   },
+  prefix: "",
   plugins: [],
 };
